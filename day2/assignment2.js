d7d89ff1071570d1679c619a0f6b02fecc1bba92
FLLAP0309 <fllap0309@FLLAP0309s-MacBook-Air.local>
2023-04-10 12:28:25 +0530

    String Operations

---
+++ day2/assignment2.js
@@ -0,0 +1,12 @@
+let challenge = '30 Days Of JavaScript';// question1
+console.log(challenge); //question2
+console.log(challenge.length); // question3
+console.log(challenge.toUpperCase()); // question4
+console.log(challenge.toLowerCase()); //question5
+console.log(challenge.substr(11,10)); // question6 
+console.log(challenge.substring(11)); // question6
+console.log(" line 8 "+challenge);
+
+console.log(challenge.includes("of")); //question8
+console.log(challenge.split(' '));  // question9
+
