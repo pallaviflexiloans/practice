e2a881357d4a290c85d874b3b2a4a4340e9c33e6
FLLAP0309 <fllap0309@FLLAP0309s-MacBook-Air.local>
2023-04-10 11:19:00 +0530

    practicing with variables

--- day1/variable.js
+++ day1/variable.js
@@ -2,3 +2,22 @@
 let a = "pallavi";
 let b = null;
 let c ; // string,null,undefined question4
+
+console.log(typeof "pallavi"); // question 5
+
+let variable1 , variable2, variable3, variable4; // question6
+
+let var1 =5, var2 =6, var3= 7, var4=8; // question7
+
+let firstname = "pallavi";
+let lastname = "sharma";
+let country = "india";
+let age = 35;
+let ismarried = true; // question8
+
+let name = "pallavi", surname = "sharma", nation = "india", yrs = 35, married = true; // question9
+
+let myAge = 35;
+let yourAge = 40; 
+console.log("my age is " + myAge );
+console.log("your age is " + yourAge); // question 10
