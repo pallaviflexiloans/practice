0743eee21cc460230b0b07c423ff74744e9a2c18
FLLAP0309 <fllap0309@FLLAP0309s-MacBook-Air.local>
2023-04-10 10:18:32 +0530

    practice variable

--- day1/assignment1.js
+++ day1/assignment1.js
@@ -3,3 +3,10 @@
 /*for(var i=0;i<5;i++){
     console.log(i)
 }*/
+
+/*
+ comments can make code readable, easy to reuse and informative */
+
+
+
+
---
+++ day1/variable.js
@@ -0,0 +1,4 @@
+
+let a = "pallavi";
+let b = null;
+let c ; // string,null,undefined question4
